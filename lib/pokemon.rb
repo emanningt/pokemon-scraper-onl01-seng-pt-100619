b19e5fab3ec5acd5dde58b7a810df1ef356db2dd
Eric Manning <emanningmusic@yahoo.com>
2020-01-08 00:13:02 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,14 @@
 class Pokemon
+  
+  attr_accessor :name, :db, :type, :id 
+  
+  def initialize(id, db, name, type)
+    @id = id 
+    @db = db 
+    @name = name
+    @type = type 
+  end 
+  
+  
+  
 end
